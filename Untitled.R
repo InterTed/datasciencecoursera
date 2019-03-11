b9a7d39241790a704745e0855b2ea577cf284bae
InterTed <t.yo@me.com>
2019-03-11 15:29:39 +0800

    course_project

---
+++ Untitled.R
@@ -0,0 +1,19 @@
+a <- available.packages()
+head (row.names(a),3) ##Show the names of the first few packages
+a
+
+install.packages("slidify")
+install.packages(c("slidify","ggplot2","devtools"))
+
+source("http://bioconductor.org/biocLite.R")
+?BiocUpgrade
+## 'http' if 'https' unavailable
+source("https://bioconductor.org/biocLite.R")
+biocLite("BiocUpgrade")
+y
+install.packages("BiocManager")
+library(ggplot2)
+search()
+
+install.packages("KernSmooth")
+library(KernSmooth)
